48421b61eafdd1c7280df54cdf45ec0332766c1c
Michiel Kamermans <pomax@nihongoresources.com>
2012-05-07 15:16:39 -0400

    more modal fixings

--- js/modals.js
+++ js/modals.js
@@ -2,61 +2,62 @@
  * webpagemaker modal dialogs
  */
 function bindModalJS() {
+  // don't leak event handlers
   document.removeEventListener("DOMContentLoaded", bindModalJS, false);
-  
+
+
+  // persistent CSS selector - since we can only have one
+  // modal dialog open at a time, we can entangle, although
+  // an object wrapping rewrite might be cleaner.
+  var defaultSelector = ".link-to-this";
+  var selector = defaultSelector;
+  var selectorImg = '';
+
+  var selectItem = function(sel) {
+    $(sel).show();
+  };
+
+  var unselectItem = function(sel) {
+    $(sel).hide();
+  };
+
+  // show the modal dialog
   $("#share_button").click(function() {
     $('#share-dialog').show();
+    selectItem(selector);
   });
-
-/*
-  // clicking outside the dialog  
-  $('#share-dialog').click(function(){
-    this.parentNode.removeChild(this);
-  });
-*/
-
-  // persistent choice
-  var selector = "";
 
   // clicking the "close" button
   $('.modal-box .close-icon').click(function(){
+    unselectItem(selector);
+    selector = defaultSelector;
     var shareDialog = this.parentNode.parentNode;
-    //shareDialog.parentNode.removeChild(shareDialog);
     $(shareDialog).hide();
-    selector = "";
   });
-  
-  var selectItem = function(a, selector) {
-    $(selector).show();
-  };
-
-  var unselectItem = function(a, selector) {
-    $(selector).hide();
-  };
 
   // content "tabs"
   $('#share-buttons a').each(function() {
+    var a = this;
+    
     // mouseover content replacements
-    $(this).mouseover(function() {
-      if (selector !== "") { unselectItem(this, selector); }
-      selector = "." + this.id;
-      selectItem(this, selector);
+    $(a).mouseover(function() {
+      if (selector !== '') {
+        if (selectorImg !== '') {
+          selectorImg.src = selectorImg.src.replace("/selected","");
+        }
+        unselectItem(selector);
+      }
+      selector = "." + a.id;
+      selectItem(selector);
+
+      selectorImg = $("img",$(a))[0];
+      selectorImg.src = selectorImg.src.replace("chopped","chopped/selected");
     });
 
     // prevent links from being resolved, since the
     // <a> elements have no href content
-    $(this).click(function() {
+    $(a).click(function() {
       return false; 
-    });
-  });
-
-  // effect content placement per button
-  $('#share-buttons img').each(function() {
-    $(this).mouseover(function() {
-      this.src = this.src.replace('chopped','chopped/selected');
-    });
-    $(this).mouseout(function() {
-      this.src=this.src.replace('/selected','');
     });
   });
 }
