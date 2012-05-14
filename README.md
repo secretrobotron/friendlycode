c23000cfc57979ca9747c4c10c7cd56382ce9331
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-05-14 19:28:57 -0400

    Add shelljs and node.js build with jshint for linting

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ to provide ultra-friendly real-time help to novice webmakers.
 
   [slowparse]: https://github.com/toolness/slowparse
   [hacktionary]: https://github.com/toolness/hacktionary
+
+To check for lint errors in the code, install node.js and npm and then:
+
+$ npm install --dev
+
+Now you can look for lint errors like so:
+
+$ node make check
---
+++ make.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var JSLINT = './node_modules/jshint/bin/hint';
+
+var files = "\
+js/editor.js \
+js/main.js \
+js/help.js \
+js/late-socials.js \
+js/publish.js \
+js/share.js \
+slowparse/slowparse.js \
+";
+
+require('shelljs/make');
+
+target.check = function() {
+  target['check-lint']();
+};
+
+target['check-lint'] = function() {
+  echo('### Linting JS files');
+
+  exec(JSLINT + ' ' + files);
+};
